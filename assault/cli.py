26e18ed02c4d09c24846121775992bf8d0b73f88
sandeepchoppara <sandeep.choppara@gmail.com>
2019-09-16 06:56:48 +0000

    Add click and create CLI

---
+++ assault/cli.py
@@ -0,0 +1,18 @@
+import click
+
+
+@click.command()
+@click.option("--requests", "-r", default=500, help="Number of requests")
+@click.option("--concurrency", "-c", default=1, help="Number of concurrent requests")
+@click.option("--json-file", "-j", default=None, help="Path to output Json File")
+@click.argument("url")
+def cli(requests, concurrency, json_file, url):
+    print(f"Requests: {requests}")
+    print(f"Concurrency: {concurrency}")
+    print(f"JSON File: {json_file}")
+    print(f"URL: {url}")
+    pass
+
+
+if __name__ == "__main__":
+    cli()
